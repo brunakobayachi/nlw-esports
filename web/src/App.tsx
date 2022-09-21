f590fe37eaebe0ea7da113730b9dad8ac1376f74
brunakobayachi <brunakobayachi@gmail.com>
2022-09-21 13:50:46 -0300

    feat aula 5

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,33 @@
 import "./styles/main.css";
+import { useState, useEffect } from "react";
 import logoImg from "./assets/logo-nlw-esports.svg";
-import { MagnifyingGlassPlus } from "phosphor-react";
+import { GameController, MagnifyingGlassPlus } from "phosphor-react";
+import { GameBanner } from "./components/GameBanner";
+import { CreateAdBanner } from "./components/CreateAdBanner";
+import { Input } from "./components/form/Input";
+import React from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+import { CreateAdModal } from "./components/CreateAdModal";
+import axios from 'axios'
+interface Game {
+    id: string;
+    title: string;
+    bannerUrl: string;
+    _count: {
+        ads: number;
+    };
+}
 
 function App() {
+    const [games, setGames] = useState<Game[]>([]);
+
+    useEffect(() => {
+        axios("http://localhost:3333/games")
+            .then(response => {
+                setGames(response.data);
+            });
+    }, []);
+
     return (
         <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
             <img src={logoImg} />
@@ -15,91 +40,23 @@ function App() {
             </h1>
 
             <div className="grid grid-cols-6 gap-6 mt-16">
-                <a href="" className="relative rounded-lg overflow-hidden">
-                    <img src="/game-1.png" alt="" />
-                    <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-                        <strong className="font-bold text-white block">
-                            League of Legends
-                        </strong>
-                        <span className="text-zinc-300 text-sm block">
-                            4 anuncios
-                        </span>
-                    </div>
-                </a>
-                <a href="" className="relative rounded-lg overflow-hidden">
-                    <img src="/game-2.png" alt="" />
-                    <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-                        <strong className="font-bold text-white block">
-                            Dota 2
-                        </strong>
-                        <span className="text-zinc-300 text-sm block">
-                            4 anuncios
-                        </span>
-                    </div>
-                </a>
-                <a href="" className="relative rounded-lg overflow-hidden">
-                    <img src="/game-3.png" alt="" />
-                    <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-                        <strong className="font-bold text-white block">
-                            Counter Strike
-                        </strong>
-                        <span className="text-zinc-300 text-sm block">
-                            4 anuncios
-                        </span>
-                    </div>
-                </a>
-                <a href="" className="relative rounded-lg overflow-hidden">
-                    <img src="/game-4.png" alt="" />
-                    <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-                        <strong className="font-bold text-white block">
-                            Apex Legends
-                        </strong>
-                        <span className="text-zinc-300 text-sm block">
-                            4 anuncios
-                        </span>
-                    </div>
-                </a>
-                <a href="" className="relative rounded-lg overflow-hidden">
-                    <img src="/game-5.png" alt="" />
-                    <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-                        <strong className="font-bold text-white block">
-                            Fortine
-                        </strong>
-                        <span className="text-zinc-300 text-sm block">
-                            4 anuncios
-                        </span>
-                    </div>
-                </a>
-                <a href="" className="relative rounded-lg overflow-hidden">
-                    <img src="/game-6.png" alt="" />
-                    <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-                        <strong className="font-bold text-white block">
-                            World of Warcraft
-                        </strong>
-                        <span className="text-zinc-300 text-sm block">
-                            4 anuncios
-                        </span>
-                    </div>
-                </a>
+                {games.map(game => {
+                    return (
+                        <GameBanner
+                            key={game.id}
+                            bannerUrl={game.bannerUrl}
+                            title={game.title}
+                            adsCount={game._count.ads}
+                        />
+                    );
+                })}
             </div>
 
-            <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
-                <div className="bg-[#2A2634] px-8 py-6 mt-8 self-stretch flex justify-between items-center">
-                    <div>
-                        <strong className="text-2xl text-white font-black block">
-                            Não encontrou seu duo?
-                        </strong>
-                        <span className="text-zinc-400 block">
-                            Publique um anúncio para encontrar novos players!
-                        </span>
-                    </div>
+            <Dialog.Root>
+                <CreateAdBanner />
 
-                    <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
-                        <MagnifyingGlassPlus size={24} />
-                        Publicar anúncio
-                    </button>
-                </div>
-            </div>
+                <CreateAdModal />
+            </Dialog.Root>
         </div>
     );
 }
